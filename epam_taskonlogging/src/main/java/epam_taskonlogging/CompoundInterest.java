5d9156f1239674052e65c658f1128a8ddabce6b9
jayanth0922 <51846496+jayanth0922@users.noreply.github.com>
2020-02-27 06:54:41 +0530

    Add files via upload

---
+++ epam_taskonlogging/src/main/java/epam_taskonlogging/CompoundInterest.java
@@ -0,0 +1,8 @@
+package epam_taskonlogging;
+
+public class CompoundInterest {
+	public double calculate_Compound_Interest(double principalAmount,double time,double rateOfInterest,double numberOfCompoundings_PerAnnum)
+	{
+		return (principalAmount*(Math.pow((1+rateOfInterest/(numberOfCompoundings_PerAnnum*100)),numberOfCompoundings_PerAnnum*time)));
+	}
+}
---
+++ epam_taskonlogging/src/main/java/epam_taskonlogging/SimpleInterest.java
@@ -0,0 +1,10 @@
+package epam_taskonlogging;
+
+public class SimpleInterest {
+
+	public double calculate_Simple_Interest(double principalAmount,double time,double rateOfInterest)
+	{
+		return principalAmount*time*rateOfInterest/100;
+	}
+
+}
